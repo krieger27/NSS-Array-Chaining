bd1a0d7350362ebe432926e14aa09f067f6c22b2
krieger27 <krieger@vols.utk.edu>
2018-01-02 08:58:26 -0600

    done on single line but think could be shorter

--- chaining.js
+++ chaining.js
@@ -5,4 +5,4 @@
 // Multiply each remaining number by 1.5 and then subtract 1.
 // Then output (either in the DOM or the console) the sum of all the resulting numbers.
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
-console.log(integers.sort());
+console.log(output.innerHTML+=integers.sort((a,b)=>{return b-a}).splice(4).map((a)=>{return a * 1.5-1}).reduce((a,b)=>{return a+b}));
